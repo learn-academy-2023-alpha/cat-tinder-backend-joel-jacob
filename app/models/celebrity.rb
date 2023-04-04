94ffc15738f52bf3ef252e535aa4ae673ff5e2e0
JacobR96 <jrosswalks@gmail.com>
2023-04-04 15:15:34 -0700

    thrid times a charm, api completed

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,10 +1,8 @@
 class Celebrity < ApplicationRecord
- validates :name, presence: true
-    validates :name, length: { minimum: 2 }
-         validates :age, presence: true
-     validates :stared_in, presence: true
-    validates :stared_in, length: { minimum:10 }
- validates :image, presence: true
-end
-
-  
+   validates :name, presence: true
+   validates :name, length: { minimum: 2 }
+   validates :age, presence: true
+   validates :stared_in, presence: true
+   validates :stared_in, length: { minimum:10 }
+   validates :image, presence: true
+end 
--- spec/requests/celebrities_spec.rb
+++ spec/requests/celebrities_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe "Celebrities", type: :request do
   describe "GET /index" do
     it "gets a list of celebrities" do
       Celebrity.create(
-          name: 'Simone Ashley',
-            age: "27",
-       stared_in: 'Bridgerton',
-      image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.today.com%2Fpopculture%2Ftv%2Fbridgerton-season-2-simone-ashley-interview-rcna21469&psig=AOvVaw1LFPGsx-uBr-L7yzMx9Ix2&ust=1680644770460000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPjjg-bXjv4CFQAAAAAdAAAAABAE'
+        name: 'Simone Ashley',
+        age: "27",
+        stared_in: 'Bridgerton',
+        image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.today.com%2Fpopculture%2Ftv%2Fbridgerton-season-2-simone-ashley-interview-rcna21469&psig=AOvVaw1LFPGsx-uBr-L7yzMx9Ix2&ust=1680644770460000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPjjg-bXjv4CFQAAAAAdAAAAABAE'
       )
-        get '/celebrities'
-        celebrity = JSON.parse(response.body)
+      get '/celebrities'
+      celebrity = JSON.parse(response.body)
 
-    expect(response).to have_http_status(200)
-    expect(celebrity.length).to eq 1
-  end
-end
+      expect(response).to have_http_status(200)
+      expect(celebrity.length).to eq 1
+    end
+ end
 
 
   describe "POST / create" do
@@ -23,9 +23,9 @@ end
       celebrity_params = {
        celebrity: {  
         name: 'Luke Newton',
-       age: "30",
-      stared_in: 'Bridgerton',
-    image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.independent.co.uk%2Farts-entertainment%2Ftv%2Ffeatures%2Fluke-newton-interview-bridgerton-season-2-b2037973.html&psig=AOvVaw0S-DtIW1tehqjB1_dA8KFm&ust=1680645077806000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNiX0PjYjv4CFQAAAAAdAAAAABAE'
+        age: "30",
+        stared_in: 'Bridgerton',
+        image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.independent.co.uk%2Farts-entertainment%2Ftv%2Ffeatures%2Fluke-newton-interview-bridgerton-season-2-b2037973.html&psig=AOvVaw0S-DtIW1tehqjB1_dA8KFm&ust=1680645077806000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNiX0PjYjv4CFQAAAAAdAAAAABAE'
        
 }
       }
@@ -37,9 +37,9 @@ end
       updated_celebrity_params = {
         celebrity: {
           name: 'Regé-Jean Page',
-            age: "35",
+          age: "35",
           stared_in: 'Bridgerton',
-        image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glamour.com%2Fstory%2Frege-jean-page-leaving-bridgerton-ok&psig=AOvVaw2ElpGua5ilaQQzR7787uAX&ust=1680644900143000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJig56PYjv4CFQAAAAAdAAAAABAE'
+          image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glamour.com%2Fstory%2Frege-jean-page-leaving-bridgerton-ok&psig=AOvVaw2ElpGua5ilaQQzR7787uAX&ust=1680644900143000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJig56PYjv4CFQAAAAAdAAAAABAE'
         }
       }
 patch "/celebrities/#{celebrity.id}", params:
@@ -55,9 +55,9 @@ patch "/celebrities/#{celebrity.id}", params:
         celebrity_params = {
          celebrity: {  
           name: 'Regé-Jean Page',
-         age: "34",
-        stared_in: 'Bridgerton',
-      image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glamour.com%2Fstory%2Frege-jean-page-leaving-bridgerton-ok&psig=AOvVaw2ElpGua5ilaQQzR7787uAX&ust=1680644900143000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJig56PYjv4CFQAAAAAdAAAAABAE'
+          age: "34",
+          stared_in: 'Bridgerton',
+          image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glamour.com%2Fstory%2Frege-jean-page-leaving-bridgerton-ok&psig=AOvVaw2ElpGua5ilaQQzR7787uAX&ust=1680644900143000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJig56PYjv4CFQAAAAAdAAAAABAE'
           }
         }
         post '/celebrities', params: celebrity_params
@@ -66,8 +66,8 @@ patch "/celebrities/#{celebrity.id}", params:
             delete "/celebrities/#{celebrity.id}"
          expect(response).to have_http_status(200)
         expect(celebrities). to be_empty
-    end
-  end
+     end
+   end
 
    describe "without a name" do 
     it "doesn't create a celebrity without a name" do
